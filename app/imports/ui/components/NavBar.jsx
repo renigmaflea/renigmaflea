7615877498a5a55c181b5916dda163166b14f1b0
Patima Poochai <ppoochai@hawaii.edu>
2020-04-16 01:49:41 -1000

    Finished mockup

--- app/imports/ui/components/NavBar.jsx
+++ app/imports/ui/components/NavBar.jsx
@@ -18,7 +18,7 @@ class NavBar extends React.Component {
         </Menu.Item>
         {this.props.currentUser ? (
             [<Menu.Item as={NavLink} activeClassName="active" exact to="/add" key='add'>Home</Menu.Item>,
-              <Menu.Item as={NavLink} activeClassName="active" exact to="/add" key='add'>Categories</Menu.Item>,
+              <Menu.Item as={NavLink} activeClassName="active" exact to="/listcat" key='add'>Categories</Menu.Item>,
               <Menu.Item as={NavLink} activeClassName="active" exact to="/add" key='add'>Post Item</Menu.Item>,
               <Menu.Item as={NavLink} activeClassName="active" exact to="/list" key='list'>About Us</Menu.Item>]
         ) : ''}
