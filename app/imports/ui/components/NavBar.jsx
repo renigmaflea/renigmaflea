868421a5deb1587eb9355f1fd9614982daee11a8
Patima Poochai <ppoochai@hawaii.edu>
2020-04-16 21:08:51 -1000

    Change app logo

--- app/imports/ui/components/NavBar.jsx
+++ app/imports/ui/components/NavBar.jsx
@@ -3,18 +3,19 @@ import PropTypes from 'prop-types';
 import { Meteor } from 'meteor/meteor';
 import { withTracker } from 'meteor/react-meteor-data';
 import { withRouter, NavLink } from 'react-router-dom';
-import { Menu, Dropdown, Header } from 'semantic-ui-react';
+import { Menu, Dropdown, Image } from 'semantic-ui-react';
 import { Roles } from 'meteor/alanning:roles';
 
 /** The NavBar appears at the top of every page. Rendered by the App Layout component. */
 class NavBar extends React.Component {
   render() {
-    const menuStyle = { marginBottom: '10px', backgroundColor: '#529575' };
+    const menuStyle = { marginBottom: '10px', backgroundColor: '#529575', fontSize: '18px' };
     const itemStyle = { margin: '0px', padding: '0px' };
     return (
       <Menu style={menuStyle} attached="top" borderless inverted>
         <Menu.Item as={NavLink} activeClassName="" exact to="/" style={itemStyle}>
-          <Header style={itemStyle} className="logo" inverted as='h1'>RenigmaFlea</Header>
+          {/* <Header style={itemStyle} className="logo" inverted as='h1'>RenigmaFlea</Header> */}
+          <Image src='/images/menu_logo.png' size='small'/>
         </Menu.Item>
         {this.props.currentUser ? (
             [<Menu.Item as={NavLink} activeClassName="active" exact to="/add" key='add'>Home</Menu.Item>,
