fb19850e6237f3af9791429306ba59f304449108
Patima Poochai <ppoochai@hawaii.edu>
2020-04-18 20:41:47 -1000

    Add ReportItemButton component
    This component is a pre-build component used to get to an item's reporting page. It could be incorporated into item listings by importing it into the jsx.

---
+++ app/imports/ui/components/ReportItemButton.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Button } from 'semantic-ui-react';
+import 'uniforms-bridge-simple-schema-2'; // required for Uniforms
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
+/** Use this component with the item's id as a parameter to report an item */
+class ReportItemButton extends React.Component {
+  render() {
+    return (
+        <Link to={`/report/${this.props.itemID}`}><Button>Report this item</Button></Link>
+    );
+  }
+}
+
+ReportItemButton.propTypes = {
+  itemID: PropTypes.string.isRequired,
+};
+
+export default ReportItemButton;
