bede211d896125ea90b74272d66ae1461670d6f3
justinhwong <justinhw@hawaii.edu>
2020-04-19 14:03:49 -1000

    Update Item.jsx

--- app/imports/ui/components/Item.jsx
+++ app/imports/ui/components/Item.jsx
@@ -5,7 +5,7 @@ import { withRouter, Link } from 'react-router-dom';
 import Note from './Note';
 import AddNote from '../components/AddNote';
 
-/** Renders a single row in the List Stuff table. See pages/ListProfile.jsx. */
+/** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
 class Item extends React.Component {
   render() {
     return (
@@ -15,16 +15,16 @@ class Item extends React.Component {
             <Image
                 floated='right'
                 size='mini'
-                src={this.props.item.image}
+                src={this.props.contact.image}
             />
-            <Card.Header>{this.props.item.firstName} {this.props.item.lastName}</Card.Header>
-            <Card.Meta>{this.props.item.address}</Card.Meta>
+            <Card.Header>{this.props.contact.firstName} {this.props.contact.lastName}</Card.Header>
+            <Card.Meta>{this.props.contact.address}</Card.Meta>
             <Card.Description>
-              {this.props.item.description} <strong></strong>
+              {this.props.contact.description} <strong></strong>
             </Card.Description>
           </Card.Content>
           <Card.Content extra>
-            <Link to={`/edit/${this.props.item._id}`}>Edit</Link>
+            <Link to={`/edit/${this.props.contact._id}`}>Edit</Link>
           </Card.Content>
           <Card.Content extra>
             <Feed>
@@ -32,7 +32,7 @@ class Item extends React.Component {
             </Feed>
           </Card.Content>
           <Card.Content extra>
-            <AddNote owner={this.props.item.owner} itemId={this.props.item._id}/>
+            <AddNote owner={this.props.contact.owner} contactId={this.props.contact._id}/>
           </Card.Content>
         </Card>
     );
@@ -41,7 +41,7 @@ class Item extends React.Component {
 
 /** Require a document to be passed to this component. */
 Item.propTypes = {
-  item: PropTypes.object.isRequired,
+  contact: PropTypes.object.isRequired,
   notes: PropTypes.array.isRequired,
 };
 
