248db1bf4c674fb2b52aaf93826dc43ebc83df98
Patima Poochai <ppoochai@hawaii.edu>
2020-04-20 22:42:37 -1000

    Commit progress to admin reporting page: - Change tab name - Add publications for Items and Reports - Install underscore and added it to dependency - add default items and reports to settings.development.json

--- app/imports/startup/server/Publications.js
+++ app/imports/startup/server/Publications.js
@@ -2,6 +2,7 @@ import { Meteor } from 'meteor/meteor';
 import { Roles } from 'meteor/alanning:roles';
 import { Items } from '../../api/item/Items';
 import { Notes } from '../../api/note/Notes';
+import { Reports } from '../../api/report/Reports';
 
 /** This subscription publishes only the documents associated with the logged in user */
 Meteor.publish('Items', function publish() {
@@ -34,6 +35,22 @@ Meteor.publish('Notes', function publish() {
   if (this.userId) {
     const username = Meteor.users.findOne(this.userId).username;
     return Notes.find({ owner: username });
+  }
+  return this.ready();
+});
+
+/** This subscription publishes all documents regardless of user, but only if the logged in user is the Admin. */
+Meteor.publish('Reports', function publish() {
+  if (this.userId && Roles.userIsInRole(this.userId, 'admin')) {
+    return Reports.find();
+  }
+  return this.ready();
+});
+
+/** This subscription publishes all documents regardless of user, but only if the logged in user is the Admin. */
+Meteor.publish('Items', function publish() {
+  if (this.userId && Roles.userIsInRole(this.userId, 'admin')) {
+    return Items.find();
   }
   return this.ready();
 });
