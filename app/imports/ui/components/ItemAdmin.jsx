f664a7ae8320a5a2e2e1570bbb244143db235c45
andrewemmons95 <60409142+andrewemmons95@users.noreply.github.com>
2020-04-28 22:20:24 -1000

    admin

--- app/imports/ui/components/ItemAdmin.jsx
+++ app/imports/ui/components/ItemAdmin.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
-import { Card, Image } from 'semantic-ui-react';
+import { Card, Image, Button } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 import { withRouter } from 'react-router-dom';
 
 /** Renders a single row in the List Stuff table. See pages/ListProfile.jsx. */
-class ItemAdmin extends React.Component {
+class Item extends React.Component {
   render() {
     return (
-        // centered maybe not needed
         <Card centered>
           <Card.Content>
             <Image
@@ -15,14 +14,23 @@ class ItemAdmin extends React.Component {
                 size='mini'
                 src={this.props.item.image}
             />
-            <Card.Header>{this.props.item.firstName} {this.props.item.lastName}</Card.Header>
-            <Card.Meta>{this.props.item.address}</Card.Meta>
+            <Card.Header>{this.props.item.firstName} {this.props.item.lastName} </Card.Header>
+            <Card.Header>{this.props.item.itemName} </Card.Header>
+            <Card.Meta>{this.props.item.address} </Card.Meta>
             <Card.Description>
-              {this.props.item.description} <strong>best friends</strong>
+              {this.props.item.description}
             </Card.Description>
-          </Card.Content>
-          <Card.Content extra>
-            {this.props.item.owner}
+            <div className='ui two buttons'>
+              <Button basic color='green'>
+                Add to basket
+              </Button>
+              <Button basic color='red'>
+                Report
+              </Button>
+            </div>
+            <Card.Content extra>
+              {this.props.item.owner}
+            </Card.Content>
           </Card.Content>
         </Card>
     );
@@ -30,9 +38,9 @@ class ItemAdmin extends React.Component {
 }
 
 /** Require a document to be passed to this component. */
-ItemAdmin.propTypes = {
+Item.propTypes = {
   item: PropTypes.object.isRequired,
 };
 
 /** Wrap this component in withRouter since we use the <Link> React Router element. */
-export default withRouter(ItemAdmin);
+export default withRouter(Item);
