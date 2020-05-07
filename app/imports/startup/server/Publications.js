d1b2b9a4e36a9f000bc84eb6a533ac67b85a30ee
Patima Poochai <ppoochai@hawaii.edu>
2020-05-06 22:08:18 -1000

    Mock up messaging page and add message collection

--- app/imports/startup/server/Publications.js
+++ app/imports/startup/server/Publications.js
@@ -3,6 +3,7 @@ import { Roles } from 'meteor/alanning:roles';
 import { Items } from '../../api/item/Items';
 import { Reports } from '../../api/report/Reports';
 import { Categories } from '../../api/categories/Categories';
+import { Messages } from '../../api/messages/Messages';
 
 /** This subscription publishes only the documents associated with the logged in user */
 Meteor.publish('Items', function publish() {
@@ -50,6 +51,24 @@ Meteor.publish('Reports', function publish() {
 Meteor.publish('Categories', function publish() {
   if (this.userId) {
     return Categories.find();
+  }
+  return this.ready();
+});
+
+/** Publishes a user's outgoing messages */
+Meteor.publish('MessagesSend', function publish() {
+  if (this.userId) {
+    const username = Meteor.users.findOne(this.userId).username;
+    return Messages.find({ sender: username });
+  }
+  return this.ready();
+});
+
+/** Publishes a user's outgoing messages */
+Meteor.publish('MessagesReceive', function publish() {
+  if (this.userId) {
+    const username = Meteor.users.findOne(this.userId).username;
+    return Messages.find({ receiver: username });
   }
   return this.ready();
 });
